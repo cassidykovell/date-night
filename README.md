4ffcb292dd59579edc2b77913a8f8b3cd9fd9ea1
Cassidy <cassidy0kovell@gmail.com>
2024-02-12 10:17:24 -0800

    added comments, added a README, and attempted to fix linking issues

--- README.md
+++ README.md
@@ -1,1 +1,29 @@
 # date-night
+
+https://cassidykovell.github.io/date-night/
+
+## Description
+
+This webpage provides a date night for its user by randomly generating both drinks and a meal for a date. 
+
+## Installation
+No notes for installation.
+
+## Usage
+In order to use this website the user must click the 'generate' button. Once the user has clicked the 'generate' button two items will appear, one with a name and photo of a drink, and one with a name and photo of a meal. If for some reason the user does not like either one or both of the options provided two buttons have been displayed to the left one to generate a new drink recipe and one to generate a new meal recipe. Once the user is satisfied with the items provided they can click the 'see recipes' button and the name, photo, and recipe of both the cocktail and the meal will be displayed for the user.
+
+![alttext](Deployed.png)
+
+## Credits
+Collaberation of Chris Lowenthal, Giovanna Domingues, and Cassidy Kovell
+
+## License
+
+## Badges
+
+## Features
+The features for this webpage inculding calling randomly from two APIs one for cocktails and one for meals and then when they are both generated they are stored in local storage so they can be displayed for the user. 
+
+## How to Contribute
+
+## Tests
--- assets/js/ingredients.js
+++ assets/js/ingredients.js
@@ -1,25 +1,30 @@
-var drinkContainer = document.getElementById('drink-container');
-var mealContainer = document.getElementById('meal-container');
+//declaring all local variables 
+var drinkContainer = document.getElementById("drink-container");
+var mealContainer = document.getElementById("meal-container");
 
+//function for the dislaying the drink that was saved in local storage in the HTML
 function displayDrink() {
-    var drinkVariable = localStorage.getItem('DrinkRecipeData');
-    var drinkData = JSON.parse(drinkVariable);
-    drinkContainer.innerHTML = `
+  var drinkVariable = localStorage.getItem("DrinkRecipeData");
+  var drinkData = JSON.parse(drinkVariable);
+  drinkContainer.innerHTML = `
         <h2>${drinkData.drink.name}</h2>
         <img src="${drinkData.drink.photo}" alt="photo" />
         <p>${drinkData.drink.recipe}</p>
     `;
 }
 
+//function for displaying the meal that was saved in local stored to the HTML
 function displayMeal() {
-    var mealVariable = localStorage.getItem('MealRecipeData');
-    var mealData = JSON.parse(mealVariable);
-    mealContainer.innerHTML = `
+  var mealVariable = localStorage.getItem("MealRecipeData");
+  var mealData = JSON.parse(mealVariable);
+  mealContainer.innerHTML = `
         <h2>${mealData.meal.name}</h2>
         <img src="${mealData.meal.photo}" alt="photo" />
         <p>${mealData.meal.recipe}</p>
     `;
 }
 
+
+//calling both functions
 displayDrink();
 displayMeal();
